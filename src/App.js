a0b77d9f3a8ac5c8fdd86d3bf39b62310c650ac8
André Lins <andrelins.t.i@gmail.com>
2024-04-16 21:56:34 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> Welcome to 2024 - 159GTI</h1>
+          <h1 className="App-title">IT`s me Mario</h1>
         </header>
       </div>
     );
